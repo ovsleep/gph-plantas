e249a28d35c0f256e7711c83e6d6d4d36120f25d
Guillermo <ovsleep@gmail.com>
2015-07-26 18:34:40 -0300

    offline

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.sendfile('./public/index.html');
+    res.sendfile('./public/index.html');
 });
 
 router.get('/backend', function (req, res) {
